2ab8c5f20eb3c26b49eb7c40cbd70e4b36b50df3
MitaliPawar94 <pawarmitali31@gmail.com>
2025-09-17 10:58:29 +0530

    Add files via upload

---
+++ LargestofThree.java
@@ -0,0 +1,15 @@
+package conditions;
+
+public class LargestofThree {
+    public static void main(String[] args) {
+        int a = 15, b = 30, c = 25;
+
+        if (a >= b && a >= c) {
+            System.out.println(a + " is the largest number");
+        } else if (b >= a && b >= c) {
+            System.out.println(b + " is the largest number");
+        } else {
+            System.out.println(c + " is the largest number");
+        }
+    }
+}
